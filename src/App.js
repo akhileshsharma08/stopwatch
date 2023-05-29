a1ef53e5c3d358a36f9e5b6eb9508919499a5929
Akhilesh Sharma <58417806+akhileshsharma08@users.noreply.github.com>
2023-05-29 10:33:29 +0530

    stopwatch in react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import { useCallback, useState } from "react";
+import "./App.css";
+
+let abr = undefined;
 
 function App() {
+  const [time, setTime] = useState(0);
+  const [flag, setFlag] = useState(false);
+
+  const HandleStart = () => {
+    console.log("watch started");
+    abr = setInterval(() => {
+      setTime((x) => x + 1);
+    }, 1000);
+    setFlag(true);
+  };
+  const HandleStop = () => {
+    console.log("watch stopped");
+    clearInterval(abr);
+    setFlag(false);
+  };
+  const HandleReset = () => {
+    setTime(0);
+    console.log("watch reset");
+    clearInterval(abr);
+    setFlag(false);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App  h-screen bg-sky-950 py-20">
+        <h1 className="text-center text-4xl text-white font-bold my-10">Stop Watch</h1>
+        <div className="watchbg py-10 flex justify-center items-center">
+          <div className="flex  text-white w-40 rounded-full">
+            <div className="flex justify-center p-2">
+              <h1 className=" text-center text-9xl">{time}</h1>
+            </div>
+          </div>
+        </div>
+        <div className="flex justify-center items-center">
+          <div className="control-buttons my-10">
+            <button
+              disabled={flag}
+              onClick={HandleStart}
+              className="bg-gray-200 hover:bg-gray-400 font-bold px-6 py-2 rounded mx-4"
+            >
+              Start
+            </button>
+
+            <button
+              onClick={HandleStop}
+              className="bg-gray-200 hover:bg-gray-400 font-bold px-6 py-2 rounded mx-4"
+            >
+              Stop
+            </button>
+
+            <button
+              onClick={HandleReset}
+              className="bg-gray-200 hover:bg-gray-400 font-bold px-6 py-2 rounded mx-4"
+            >
+              Reset
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
